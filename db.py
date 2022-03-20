11afd68fe704ea4a103d53d85284d3ec8f8146e4
Incendium <50025292+1ncendium@users.noreply.github.com>
2022-03-20 13:50:44 +0100

    Added login system.

--- db.py
+++ db.py
@@ -1,11 +1,17 @@
 import sqlite3
-Gebruikersnaam = input("Gebruikersnaam: ")
-Wachtwoord = input("Wachtwoord: ")
-db = sqlite3.connect("bungelow.db")
-cursor = db.cursor()
-statement = f"Select gebruikersnaam FROM gast WHERE gebruikersnaam=? and wachtwoord=?;"
-cursor.execute(statement, [Gebruikersnaam, Wachtwoord])
-if not cursor.fetchone():
-    print("Login failed")
-else:
-    print(f"Welcome {Gebruikersnaam}")
+
+# <!------- Begin login function --------!>
+
+def login(gebruikersnaam, wachtwoord):
+    Gebruikersnaam = gebruikersnaam
+    Wachtwoord = wachtwoord
+    db = sqlite3.connect("bungelow.db")
+    cursor = db.cursor()
+    statement = f"Select gebruikersnaam FROM gast WHERE gebruikersnaam=? and wachtwoord=?;"
+    cursor.execute(statement, [Gebruikersnaam, Wachtwoord])
+    if not cursor.fetchone():
+        return True
+    else:
+        return False
+
+# <!------- End login function --------!>
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-
+from db import login
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VerySecret'
 
-class LoginForm(FlaskForm):
-    gebruikersnaam =  StringField('gebruikersnaam', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 # ------- Begin of routes ---------
 
 @app.route('/', methods=['GET'])
@@ -33,13 +28,22 @@ def aanbod():
     return render_template('aanbod.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login_user():
     """
     Page function for auth/login.html.
     Returns the login.html page trough render_template.
     Accepts POST and GET requests.
     """
-    return render_template('login.html')
+
+    error = "Foute gebruikersnaam / wachtwoord!"
+
+    if request.method == 'POST' and request.form['gebruikersnaam'] != '':
+        if login(request.form['gebruikersnaam'], request.form['wachtwoord']) == True:
+            return render_template('login.html', error=error)
+        else:
+            return render_template('index.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/registreren', methods=['GET', 'POST'])
 def register():
@@ -48,8 +52,9 @@ def register():
     Returns the register.html page trough render_template.
     Accepts POST and GET requests.
     """ 
-    return render_template('register.html')
+    return render_template('registreren.html')
 
 # ------- End of routes ---------
+
 if __name__ == "__main__":
     app.run(debug=True)
