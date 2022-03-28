8e10ee110dd6b2a757b004492cc2a9c64766eabd
Incendium <anonymous201355@gmail.com>
2022-03-28 20:53:43 +0200

    inlog systeem toegevoegd

---
+++ app.py
@@ -0,0 +1,80 @@
+from mijnproject import app, db
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from mijnproject.models import User
+from mijnproject.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/aanbod')
+@login_required
+def welkom():
+    user = current_user.username
+    return render_template('aanbod.html', user=user)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Je bent nu uitgelogd!', 'info')
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Grab the user from our User Models table
+        user = User.query.filter_by(email=form.email.data).first()
+
+        # Check that the user was supplied and the password is right
+        # The verify_password method comes from the User object
+        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
+
+        if user.check_password(form.password.data) and user is not None:
+            # Log in the user
+
+            login_user(user)
+            flash('Succesvol ingelogd!.')
+
+            # If a user was trying to visit a page that requires a login
+            # flask saves that URL as 'next'.
+            next = request.args.get('next')
+
+            # So let's now check if that next exists, otherwise we'll go to
+            # the welcome page.
+            if next == None or not next[0] == '/':
+                next = url_for('welkom')
+
+            return redirect(next)
+    return render_template('login.html', form=form)
+
+
+@app.route('/registreren', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        print(True)
+        try:
+            user = User(email=form.email.data,
+                        username=form.username.data,
+                        password=form.password.data)
+
+            db.session.add(user)
+            db.session.commit()
+            flash('Dank voor de registratie. Er kan nu ingelogd worden! ')
+            return redirect(url_for('login'))
+        except Exception as e:
+            print(e)
+    return render_template('registreren.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mijnproject/models.py
@@ -0,0 +1,33 @@
+from mijnproject import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+# De user_loader decorator zorgt voor de flask-login voor de huidige gebruiker
+# en haalt zijn/haar id op.
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
+    # Maak een tabel aan in de database
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Bungalow(db.Model):
+    id = 
+
+db.create_all()
