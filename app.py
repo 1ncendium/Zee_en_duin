6c071db8d23ed582e9bb5d2598c3430564faf09f
Incendium <anonymous201355@gmail.com>
2022-04-04 20:55:24 +0200

    updates

--- app.py
+++ app.py
@@ -79,23 +79,25 @@ def login():
 
         # Check that the user was supplied and the password is right
         # The verify_password method comes from the User object
+        if user != None:
+            if user.check_password(form.password.data):
+                # Log in the user
 
-        if user.check_password(form.password.data) and user is not None:
-            # Log in the user
+                login_user(user)
+                flash('Succesvol ingelogd!.')
 
-            login_user(user)
-            flash('Succesvol ingelogd!.')
+                # If a user was trying to visit a page that requires a login
+                # flask saves that URL as 'next'.
+                next = request.args.get('next')
 
-            # If a user was trying to visit a page that requires a login
-            # flask saves that URL as 'next'.
-            next = request.args.get('next')
+                # So let's now check if that next exists, otherwise we'll go to
+                # the aanbod page.
+                if next == None or not next[0] == '/':
+                    next = url_for('home')
 
-            # So let's now check if that next exists, otherwise we'll go to
-            # the aanbod page.
-            if next == None or not next[0] == '/':
-                next = url_for('home')
-
-            return redirect(next)
+                return redirect(next)
+        else:
+            flash("Gebruiker bestaat niet")
     return render_template('login.html', form=form)
 
 
@@ -158,22 +160,25 @@ def bedankt():
             
     return render_template('bedankt.html')
 
-@app.route('/mijn-boekingen')
+@app.route('/mijn-boekingen', methods=['POST', 'GET'])
 @login_required
 def mijnBoekingen():
-
     # Vraag id op van de gebruiker
     user = User.query.filter_by(username=current_user.username).all()[0]
     user_id = user.id
 
     # Zoek boekingen van deze gebruiker
     aantal = Boeking.query.filter_by(gast=user_id).all()
-
+    print(f'aantal = {aantal}')
     # Maak lege lijsten van alle tabellen die een boeking heeft
+    naam = []
     van = []
     tot = []
     bungalow_id = []
     prijs = []
+    afbeelding = []
+    omschrijving = []
+    boeking_id = []
 
     # Voor alle boekingen in de breedte van de boekingen en in de lengte van het aantal, append deze boeking aan de lijsten.
     for i in range(len(aantal)):
@@ -182,9 +187,35 @@ def mijnBoekingen():
         tot.append(x.tot)
         bungalow_id.append(x.bungalow)
         prijs.append(x.prijs)
+        boeking_id.append(x.id)
 
-    print(van, prijs, bungalow_id, tot)
-    return user.username
+        # Nu we alle boekingen weten van onze gast, kunnen we bij elke boeking de juiste bungalow informatie opvragen.
+        aantal_b = Bungalow.query.filter_by(id=x.id).all()
+        for j in range(len(aantal_b)):
+            y = Bungalow.query.filter_by(id=x.bungalow).all()[j]
+            naam.append(y.naam)
+            afbeelding.append(y.afbeelding)
+            omschrijving.append(y.beschrijving)
+
+
+    # Vang annuleer request op en verwerk in database
+    if request.method == 'POST':
+        if request.form["Annuleren"]:
+            annuleer_id = request.form["Annuleren"]
+            boeking = Boeking.query.get(annuleer_id)
+            db.session.delete(boeking)
+            db.session.commit()
+            return redirect(url_for('mijnBoekingen'))
+            
+
+    # Controleer of er boekingen zijn voor onze gast. Als dat niet zo is wordt nul_boekingen True.
+    aantal_b = len(aantal)
+    if aantal_b == 0:
+        nul_boekingen = True
+    else:
+        nul_boekingen = False
+
+    return render_template('mijn_boeking.html', nul_boekingen=nul_boekingen, boeking_id=boeking_id, prijs=prijs, aantal=aantal_b, naam=naam, afbeelding=afbeelding, omschrijving=omschrijving, van=van, tot=tot)
 
 @app.route('/favicon.ico')
 def favicon():
